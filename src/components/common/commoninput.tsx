cc82bb4618ebed5c41b3ec3703aeab299a7de6e0
developNeoguri <developneoguri@gmail.com>
2023-01-30 01:58:18 +0900

    Fix: CommonInput 컴포넌트에 onChange 이벤트 사용 시 타입 에러가 뜨는 문제 수정 #2
    - CommonInput 컴포넌트에 onChange Prop이 없어 발생한 이슈였습니다.
    - 관련 Prop 추가하여 문제 수정했습니다.

--- src/components/common/commoninput.tsx
+++ src/components/common/commoninput.tsx
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 import { TypedCommonInput } from "../../utils/typealies";
+import { CommonColumnBox } from "./commonui";
 
 const CommonInput = (props: TypedCommonInput) => {
   return (
-    <>
-      <STCommonLabel htmlFor={props.id}></STCommonLabel>
+    <CommonColumnBox>
+      <STCommonLabel htmlFor={props.id}>{props.title}</STCommonLabel>
       <STCommonInput
         id={props.id}
         value={props.value}
         type={props.type}
         placeholder={props.placeholder}
         maxLength={props.maxLength}
+        onChange={props.onChange}
       />
-    </>
+    </CommonColumnBox>
   );
 };
 
