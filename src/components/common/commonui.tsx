ae7de63ae64edb4822b76972e1cdff1157a1ec17
developNeoguri <developneoguri@gmail.com>
2023-02-06 01:54:34 +0900

    Fix & Add: 일부 공용 컴포넌트 수정 및 공용 Header 컴포넌트 추가 #2
    - 관리자/이용자 페이지 상단에 배치될 공용 Header 컴포넌트(CommonHeader) 추가
    - 구분선, 불투명 배경으로 사용할 공용 컴포넌트(CommonBorder, CommonBlackBackground) 추가
    - CommonRowBox, CommomColumnBox 내 조정 가능한 prop 추가
    - CommonButton, CommonInput 컴포넌트와 관련하여 사용 가능한 prop 추가 및 배치

--- src/components/common/commonui.tsx
+++ src/components/common/commonui.tsx
@@ -8,6 +8,8 @@ export const CommonColumnBox = styled.div<StyledCommonType>`
   align-items: ${(props) => props.alignItems || "normal"};
   padding: ${(props) => props.padding || "0"};
   margin: ${(props) => props.margin || "0"};
+  width: ${(props) => props.width};
+  gap: ${(props) => props.gap}};
 `;
 
 export const CommonRowBox = styled.div<StyledCommonType>`
@@ -17,4 +19,25 @@ export const CommonRowBox = styled.div<StyledCommonType>`
   align-items: ${(props) => props.alignItems || "normal"};
   padding: ${(props) => props.padding || "0"};
   margin: ${(props) => props.margin || "0"};
+  width: ${(props) => props.width};
+  gap: ${(props) => props.gap}};
+`;
+
+export const CommonBorder = styled.div<StyledCommonType>`
+  display: block;
+  background: #d4d4d4;
+  height: 3px;
+  border-radius: 1rem;
+  box-sizing: border-box;
+  margin: 2rem 1rem;
+`;
+
+export const CommonBlackBackground = styled.div`
+  background: rgba(38, 38, 38, 40%);
+  display: block;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  z-index: 5;
 `;
