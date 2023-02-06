ec51a672b478169fd39baceedb174e6d694c965a
developNeoguri <developneoguri@gmail.com>
2023-02-07 02:05:45 +0900

    - Fix: Chakra UI 도입에 따른 스타일링 구현 사항 변경 #7
    - styled-components로 구현한 스타일링을 Chakra UI로 변경
    - 기존 styled 컴포넌트들을 Chakra UI 내 컴포넌트로 수정 및 반영
    - 변경과 관련하여 체크 박스 등의 체크 여부 사항 등 일부 로직 수정

--- src/components/features/waitingform/checkdatamodal.tsx
+++ src/components/features/waitingform/checkdatamodal.tsx
@@ -1,151 +1,183 @@
 import React, { Dispatch, SetStateAction } from "react";
 import { UserData } from "../../../utils/typealies";
-import styled from "styled-components";
 import {
-  CommonBlackBackground,
-  CommonColumnBox,
-  CommonRowBox,
-} from "../../common/commonui";
-import { StCommonLabel } from "../../common/commoninput";
-import CommonButton from "../../common/commonbutton";
+  Box,
+  Flex,
+  FormLabel,
+  Heading,
+  Text,
+  Button,
+  ListItem,
+  ListIcon,
+  UnorderedList,
+} from "@chakra-ui/react";
+import { CheckCircleIcon } from "@chakra-ui/icons";
 
 const CheckDataModal: React.FC<{
   userInfo: UserData;
   close: Dispatch<SetStateAction<boolean>>;
 }> = ({ userInfo, close }) => {
   return (
-    <>
-      <CommonBlackBackground>
-        <StModalContainer>
-          <h2
-            style={{
-              fontSize: "1.25rem",
-              textAlign: "center",
-              marginBottom: "1.5rem",
-            }}
+    <Box
+      background="rgba(38, 38, 38, 40%)"
+      display="block"
+      width="100vw"
+      height="100vh"
+      position="fixed"
+      top="0"
+      zIndex="5"
+    >
+      <Flex
+        direction="column"
+        background="#ffffff"
+        margin="10vh 1rem"
+        padding="3rem 1.5rem"
+        justify="center"
+        borderRadius="0.5rem"
+      >
+        <Heading
+          as="h2"
+          fontSize="1.25rem"
+          textAlign="center"
+          marginBottom="1.5rem"
+        >
+          작성 내용을 확인해주세요.
+        </Heading>
+        <Flex direction="column">
+          <Flex
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            margin="0.5rem 0"
           >
-            작성 내용을 확인해주세요.
-          </h2>
-          <CommonColumnBox>
-            <CommonRowBox
-              justifyContent="space-between"
-              alignItems="center"
-              margin="0.5rem 0"
-            >
-              <StCommonLabel>성함</StCommonLabel>
-              <span
-                style={{
-                  fontSize: "1.25rem",
-                  color: "#58a6dc",
-                  fontWeight: "600",
-                }}
-              >
-                {userInfo.name}
-              </span>
-            </CommonRowBox>
-            <CommonRowBox
-              justifyContent="space-between"
-              alignItems="center"
-              margin="0.5rem 0"
-            >
-              <StCommonLabel>연락처</StCommonLabel>
-              <span
-                style={{
-                  fontSize: "1.25rem",
-                  color: "#58a6dc",
-                  fontWeight: "600",
-                }}
-              >
-                {" "}
-                {userInfo.tel}
-              </span>
-            </CommonRowBox>
-            <CommonRowBox
-              justifyContent="space-between"
-              alignItems="center"
-              margin="0.5rem 0"
-            >
-              <StCommonLabel>인원</StCommonLabel>
-              <span
-                style={{
-                  fontSize: "1.25rem",
-                  color: "#58a6dc",
-                  fontWeight: "600",
-                }}
-              >
-                {userInfo.member}명
-              </span>
-            </CommonRowBox>
-            {userInfo.pet === true || userInfo.child === true ? (
-              <CommonColumnBox margin="0.5rem 0">
-                <StCommonLabel>옵션</StCommonLabel>
-                <CommonColumnBox>
-                  <ul style={{ margin: "0 2rem" }}>
-                    {userInfo.child === true ? (
-                      <li style={{ fontSize: "1.5rem", margin: "0.5rem 0" }}>
-                        <span style={{ color: "#58a6dc", fontWeight: "600" }}>
-                          아이
-                        </span>
-                        가 있어요.
-                      </li>
-                    ) : (
-                      <></>
-                    )}
-                    {userInfo.pet === true ? (
-                      <li style={{ fontSize: "1.5rem", margin: "0.5rem 0" }}>
-                        <span style={{ color: "#58a6dc", fontWeight: "600" }}>
-                          반려 동물
-                        </span>
-                        이 있어요.
-                      </li>
-                    ) : (
-                      <></>
-                    )}
-                  </ul>
-                </CommonColumnBox>
-              </CommonColumnBox>
-            ) : (
-              <></>
-            )}
-          </CommonColumnBox>
-          <CommonRowBox gap="1rem" margin="1rem 0 0 0">
-            <CommonButton
-              type="button"
-              background="#58a6dc"
-              color="#ffffff"
-              border="1px solid gray"
-              borderRadius="0.25rem"
-              padding="1rem"
+            <FormLabel
               fontSize="1.25rem"
+              fontWeight="500"
+              width="30%"
+              margin="0"
             >
-              맞습니다
-            </CommonButton>
-            <CommonButton
-              type="button"
-              background="#5a5a5a"
-              color="#ffffff"
-              border="1px solid gray"
-              borderRadius="0.25rem"
-              padding="1rem"
-              onClick={() => close(false)}
+              성함
+            </FormLabel>
+            <Text fontSize="1.25rem" color="#58a6dc" fontWeight="600">
+              {userInfo.name}
+            </Text>
+          </Flex>
+          <Flex
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            margin="0.5rem 0"
+          >
+            <FormLabel
               fontSize="1.25rem"
+              fontWeight="500"
+              width="30%"
+              margin="0"
             >
-              아니에요
-            </CommonButton>
-          </CommonRowBox>
-        </StModalContainer>
-      </CommonBlackBackground>
-    </>
+              연락처
+            </FormLabel>
+            <Text fontSize="1.25rem" color="#58a6dc" fontWeight="600">
+              {" "}
+              {userInfo.tel}
+            </Text>
+          </Flex>
+          <Flex
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            margin="0.5rem 0"
+          >
+            <FormLabel
+              fontSize="1.25rem"
+              fontWeight="500"
+              width="30%"
+              margin="0"
+            >
+              인원
+            </FormLabel>
+            <Text fontSize="1.25rem" color="#58a6dc" fontWeight="600">
+              {userInfo.member}명
+            </Text>
+          </Flex>
+          {userInfo.pet === true || userInfo.child === true ? (
+            <Flex direction="column" margin="0.5rem 0">
+              <FormLabel
+                fontSize="1.25rem"
+                fontWeight="500"
+                width="30%"
+                margin="0"
+              >
+                옵션
+              </FormLabel>
+              <Flex direction="column">
+                <UnorderedList>
+                  {userInfo.child === true ? (
+                    <ListItem
+                      display="flex"
+                      flexDirection="row"
+                      alignItems="center"
+                      fontSize="1.25rem"
+                      margin="1rem 0"
+                    >
+                      <ListIcon as={CheckCircleIcon} />
+                      <Text color="#58a6dc" fontWeight="600">
+                        아이
+                      </Text>
+                      가 있어요.
+                    </ListItem>
+                  ) : (
+                    <></>
+                  )}
+                  {userInfo.pet === true ? (
+                    <ListItem
+                      display="flex"
+                      flexDirection="row"
+                      alignItems="center"
+                      fontSize="1.25rem"
+                      margin="1rem 0"
+                    >
+                      <ListIcon as={CheckCircleIcon} />
+                      <Text color="#58a6dc" fontWeight="600">
+                        반려 동물
+                      </Text>
+                      이 있어요.
+                    </ListItem>
+                  ) : (
+                    <></>
+                  )}
+                </UnorderedList>
+              </Flex>
+            </Flex>
+          ) : (
+            <></>
+          )}
+        </Flex>
+        <Flex direction="row" gap="1rem" margin="1rem 0 0 0" justify="center">
+          <Button
+            type="button"
+            background="#58a6dc"
+            color="#ffffff"
+            padding="1.5rem"
+            borderRadius="0.25rem"
+            fontSize="1.25rem"
+          >
+            맞습니다
+          </Button>
+          <Button
+            type="button"
+            background="#5a5a5a"
+            color="#ffffff"
+            padding="1.5rem"
+            borderRadius="0.25rem"
+            onClick={() => close(false)}
+            fontSize="1.25rem"
+          >
+            아니에요
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
-export default CheckDataModal;
 
-const StModalContainer = styled.div`
-  background: #ffffff;
-  margin: 10vh 1rem;
-  padding: 3rem 1.5rem;
-  display: flex;
-  flex-direction: column;
-  justify-contents: center;
-  border-radius: 0.5rem;
-`;
+export default CheckDataModal;
