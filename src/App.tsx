48d8a1af00a3f29118303dcff4b9136cabd42ff0
developNeoguri <developneoguri@gmail.com>
2023-02-07 23:45:50 +0900

    Fix & Add : RecoilRoot 배치, 공용 폰트인 '프리텐다드' 적용 #1
    - App.tsx에 Recoil 라이브러리를 사용하기 위한 RecoilRoot 배치
    - index.css 파일 내에 프리텐다드 폰트 관련 모듈 import 및 font-family에 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Router from "./router/router";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./utils/chakraextend";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Router />
+      <RecoilRoot>
+        <Router />
+      </RecoilRoot>
     </ChakraProvider>
   );
 }
