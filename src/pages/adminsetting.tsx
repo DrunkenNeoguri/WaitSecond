111273cc56d7e12d69eb009d9555c33680eef299
developNeoguri <developneoguri@gmail.com>
2023-04-25 18:39:55 +0900

    Fix: 로그인 상태가 아니어도 일부 관리자 페이지에 접근할 수 있는 문제 수정 #58
    - useNavigate가 최초 렌더링 전에는 작동되지 않는다는 점을 확인.
    - 렌더링 후에 navigate 기능이 작동될 수 있도록 useEffect를 적용해 최초 렌더링 후에 로그인 상태를 확인할 수 있도록 수정

--- src/pages/adminsetting.tsx
+++ src/pages/adminsetting.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { useToast } from "@chakra-ui/react";
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -13,20 +14,22 @@ function AdminSetting() {
   const navigate = useNavigate();
   const toastMsg = useToast();
 
-  if (loginStateCheck() === false) {
-    if (toastMsg.isActive("error-notLogin")) {
-      toastMsg({
-        title: "로그인 상태가 아님",
-        id: "error-notLogin",
-        description:
-          "로그인 상태가 아닙니다. 로그인 페이지에서 정상적으로 로그인해주세요.",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+  useEffect(() => {
+    if (loginStateCheck() === false) {
+      if (toastMsg.isActive("error-notLogin")) {
+        toastMsg({
+          title: "로그인 상태가 아님",
+          id: "error-notLogin",
+          description:
+            "로그인 상태가 아닙니다. 로그인 페이지에서 정상적으로 로그인해주세요.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+      navigate("/adminlogin");
     }
-    navigate("/adminlogin");
-  }
+  }, []);
 
   return (
     <AdminPage>
--- src/pages/adminwaitinglist.tsx
+++ src/pages/adminwaitinglist.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { useToast } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import CommonHeader from "../components/common/commonheader";
@@ -9,20 +10,22 @@ function AdminWaitingList() {
   const navigate = useNavigate();
   const toastMsg = useToast();
 
-  if (loginStateCheck() === false) {
-    if (toastMsg.isActive("error-notLogin")) {
-      toastMsg({
-        title: "로그인 상태가 아님",
-        id: "error-notLogin",
-        description:
-          "로그인 상태가 아닙니다. 로그인 페이지에서 정상적으로 로그인해주세요.",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+  useEffect(() => {
+    if (loginStateCheck() === false) {
+      if (toastMsg.isActive("error-notLogin")) {
+        toastMsg({
+          title: "로그인 상태가 아님",
+          id: "error-notLogin",
+          description:
+            "로그인 상태가 아닙니다. 로그인 페이지에서 정상적으로 로그인해주세요.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+      navigate("/adminlogin");
     }
-    navigate("/adminlogin");
-  }
+  }, []);
 
   return (
     <AdminPage>
